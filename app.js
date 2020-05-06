78b95404abaf01d007003476f15b95af84db8260
RaiPeter <rai.peter2@gmail.com>
2020-05-07 00:09:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Importing the modules needed for creating the app
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+var path = require('path')
+
